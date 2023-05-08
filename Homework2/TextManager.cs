d0e9f7d2b38310b58b77f5debe15f850b5503614
Michal <m.stefanski98@gmail.com>
2023-05-08 09:55:30 +0200

    Implementation from classes

---
+++ Homework2/TextManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class TextManager
+    {
+        public string FindLongestWord(string sentence)
+        {
+
+            string[] words = sentence.Split(' ');
+            string currentLongest = string.Empty;
+
+            foreach (string word in words)
+            {
+                if (word.Length > currentLongest.Length)
+                    currentLongest = word;
+            }
+            return currentLongest;
+        }
+
+        public string[] FindAllLongestWords(string sentence)
+        {
+            string longest = FindLongestWord(sentence);
+            return FindWordsOfGivenLength(longest.Length, sentence);
+
+        }
+
+        private string[] FindWordsOfGivenLength(int length, string sentence)
+        {
+            var foundWords = new List<string>();
+            foreach (var word in sentence.Split(' '))
+            {
+                if(word.Length == length)
+                    foundWords.Add(word);
+
+            }
+            return foundWords.ToArray();
+        }
+    }
+}
